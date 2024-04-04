c0d56fefe7148377e493f2fb9aa3df0004cf5c3a
unknown <anabogoevska8@gmail.com>
2024-04-04 19:23:53 +0200

    First commit

---
+++ EShopAppAdmin/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShopAppAdmin.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+
+        public string ProductImage { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public double ProductPrice { get; set; }
+
+        public double ProductRating { get; set; }
+    }
+}
---
+++ EShopAppAdmin/Models/ProductInOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShopAppAdmin.Models
+{
+    public class ProductInOrder
+    {
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+        public Guid OrderId { get; set; }
+        //public Order Order { get; set; }
+        public int Quantity { get; set; }
+    }
+}
